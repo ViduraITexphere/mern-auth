32532c6c6ab02169de7e150e71da92d07db52dda
vidura-kaushalya <vidura.in@gmail.com>
2023-12-22 13:07:03 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-blue-950 font-bold text-2xl flex justify-center ">
+        Hello World 🚀
+      </h1>
+    </>
+  );
+}
+
+export default App;
