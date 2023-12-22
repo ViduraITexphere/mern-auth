a08916cbf33425536d393e7b34c7f30490f4d51f
vidura-kaushalya <vidura.in@gmail.com>
2023-12-22 14:29:22 +0530

    connect to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
