65068b712f3907406537a2dba927ebd5b8ce90b2
vidura-kaushalya <vidura.in@gmail.com>
2023-12-25 13:42:39 +0530

    complete login

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <h1 className="font-bold">Auth App</h1>
         <ul className="flex gap-7 font-semibold">
           <Link to="/">Home</Link>
-          <Link to="/signin">Signin</Link>
           <Link to="/profile">Profile</Link>
           <Link to="/about">About</Link>
+          <Link to="/signin">
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                alt="profile"
+                className="h-7 w-7 rounded-full object-cover"
+              />
+            ) : (
+              <li>Sign In</li>
+            )}
+          </Link>
         </ul>
       </div>
     </div>
